4f5d5349f13e40cae627113c1f3987fad84ff048
kilian <kilianrave@gmail.com>
2021-10-01 11:23:08 +0200

    import

--- Calculations.py
+++ Calculations.py
@@ -1,10 +1,11 @@
-
+import math
+import input_parameters
 
 def SNR():
     k = 228.6       # Boltzmann constant
     snr = P + L_l + G_t + L_a + G_r + L_s + L_pr + L_r + k - R - T_s
     return snr
 
-def decibel_converter(value):   #  Convert any value to decibels
-    X = math.log(value/1, 10)
+def decibel_converter(value, Xref):   #  Convert any value to decibels
+    X = math.log(value/Xref, 10)
     return X
