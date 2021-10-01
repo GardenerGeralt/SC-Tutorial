2716218a96dc11215350b025a9beada5c62f3771
kilian <kilianrave@gmail.com>
2021-10-01 11:18:29 +0200

    decibel

--- Calculations.py
+++ Calculations.py
@@ -5,3 +5,6 @@ def SNR():
     snr = P + L_l + G_t + L_a + G_r + L_s + L_pr + L_r + k - R - T_s
     return snr
 
+def decibel_converter(value):   #  Convert any value to decibels
+    X = math.log(value/1, 10)
+    return X
