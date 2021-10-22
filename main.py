06ddfd2fe25fef1ad69a5fc3a97d6ec41e2b1693
kilian <kilianrave@gmail.com>
2021-10-22 15:13:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import input_parameters as i
 
 def main():
     i.ud = input("Uplink or Downlink? (u/d): ")
-    if i.ud == "u":
-        _ = i.e_t_t, i.e_t_r
-        i.e_t_r, i.e_t_t = _
 
     it = input("Iterate? (y/n) ")
     if it == "y":
